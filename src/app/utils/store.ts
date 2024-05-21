75584919e8a950165d6dc8625fe22f35b373eb5f
Julian Tibana <julian.tibanal@bam.com.gt>
2024-05-20 20:13:11 -0500

    update en nombres y descripciones de los productos

--- src/app/utils/store.ts
+++ src/app/utils/store.ts
@@ -3,36 +3,36 @@
 export const products: Product[] = [
   {
     id: 1,
-    name: 'Product 1',
-    description: 'Description 1',
+    name: 'Banana',
+    description: 'Fruta tropical colombiana',
     price: 100,
     quantity: 2
   },
   {
     id: 2,
-    name: 'Product 2',
-    description: 'Description 2',
+    name: 'Fresa',
+    description: 'Fruta dulce y rica en vitamina C',
     price: 200,
     quantity: 1
   },
   {
     id: 3,
-    name: 'Product 3',
-    description: 'Description 3',
+    name: 'Cebolla',
+    description: 'Vegetal perfecto para tus comidas',
     price: 300,
     quantity: 1
   },
   {
     id: 4,
-    name: 'Product 4',
-    description: 'Description 4',
+    name: 'Apio',
+    description: 'Vegetal perfecto para batidos',
     price: 400,
     quantity: 1
   },
   {
     id: 5,
-    name: 'Product 5',
-    description: 'Description 5',
+    name: 'Mango',
+    description: 'Fruta deliciosa y tropical',
     price: 500,
     quantity: 1
   }
